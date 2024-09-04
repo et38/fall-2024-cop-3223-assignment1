446d52aaebcd9b698a0d5ce4fc1d2cf1318a6bd1
et38 <et380833@ucf.edu>
2024-09-04 19:28:50 +0000

    framework made

---
+++ fracturing.c
@@ -0,0 +1,75 @@
+//********************************************************
+// fracturing.c
+// Author: Ethan Owen
+// UCFID: 5225596
+// Date: 9/29/2024
+// Class: COP 3223, Professor Parra
+// Purpose: This program . The main purpose of this assignment is to .
+// Input: 
+//
+// Output: 
+//********************************************************
+
+// /workspaces/fall-2024-cop-3223-assignment1/fracturing.c && ./a.out
+printf("");
+
+// Includes libraries required for code to function
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char **argv);
+{
+
+
+    calculateDistance();
+    calculatePerimeter();
+    calculateArea();
+    calculateWidth();
+    calculateHeight();
+
+    return 0;
+}
+
+double calculateDistance();
+{
+    printf("Point #1 entered: x1 = -whatever was entered-; y1 = -whatever was entered-");
+    printf("Point #2 entered: x2 = -whatever was entered-; y2 = -whatever was entered-");
+    printf("The distance between the two points is -whatever is computed-");
+    return 0;
+    //Return: A double representing the distance
+}
+
+double calculatePerimeter();
+{
+    printf("Point #1 entered: x1 = -whatever was entered-; y1 = -whatever was entered-");
+    printf("Point #2 entered: x2 = -whatever was entered-; y2 = -whatever was entered-");
+    return 0;
+
+    //Return: A double indicating how difficult you found to do this function on a scale of 1.0 through 5.0 where 1 is easy and 5 is hard
+}
+
+double calculateArea();
+{
+    printf("");
+    printf("");
+    printf("");
+    return 0;
+}
+
+double calculateWidth();
+{
+    printf("");
+    printf("");
+    printf("");
+    return 0;
+}
+
+double calculateHeight(); 
+{
+    printf("");
+    printf("");
+    printf("");
+    return 0;
+}
